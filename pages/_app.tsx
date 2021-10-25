c7e3b663ee773f6c17ad93f06a30dd463be9316f
avatarfreak <avatarfreak83@gmail.com>
2021-10-25 17:07:31 +0530

    added meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,12 @@ import "windi.css";
 import Head from "next/head";
 import { DataProvider } from "@/context/dataContext";
 import { CartProvider } from "provider/CartProvider";
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="E-commerce product page challenged by avatarfreak" />
-        <link
-          rel="shortcut icon"
-          href={`${prefix}/images/favicon-32x32.icon`}
-          type="image/x-icon"
-        />
-
         <title>Frontend Mentor | E-commerce product page</title>
       </Head>
       <CartProvider>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,17 +6,22 @@ class MyDocument extends Document {
     return initialProps;
   }
   render() {
+    const prefix = process.env.NEXT_PUBLIC_BASE_PATH;
     return (
       <Html lang="en">
         <Head>
-          <meta charSet="UTF-8" />
-          <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon-32x32.png" />
+          <meta name="description" content="E-commerce product page challenged by avatarfreak" />
+          <link
+            rel="shortcut icon"
+            href={`${prefix}/images/favicon-32x32.icon`}
+            type="image/x-icon"
+          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700&display=swap"
             rel="stylesheet"
-          />{" "}
+          />
         </Head>
         <body>
           <Main />
