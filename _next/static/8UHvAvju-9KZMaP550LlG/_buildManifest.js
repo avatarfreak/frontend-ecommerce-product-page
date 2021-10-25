1d3fba56a3cfa0110a0176935cbc970e76d0dfcb
avatarfreak <avatarfreak83@gmail.com>
2021-10-25 11:21:22 +0000

    Deploying to gh-pages from @ avatarfreak/frontend-ecommerce-product-page@5e4786e2d32044680962893806654d46a38346c1 🚀

---
+++ _next/static/8UHvAvju-9KZMaP550LlG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2949aa39a7de2ff063da.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
