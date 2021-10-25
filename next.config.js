7ae25a81f52ba815e60b0a71f51285fbfebad8d9
avatarfreak <avatarfreak83@gmail.com>
2021-10-25 15:20:08 +0530

    added base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const WindiCSS = require("windicss-webpack-plugin").default;
 
 module.exports = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   trailingSlash: true,
   webpack(config) {
