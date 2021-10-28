1e3d3fa8506e9792918fe3d1380cfe9acab5aa85
avatarfreak <avatarfreak83@gmail.com>
2021-10-28 14:24:26 +0530

    add animation stoper class

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "windi.css";
@@ -6,18 +7,33 @@ import { DataProvider } from "@/context/dataContext";
 import { CartProvider } from "provider/CartProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Frontend Mentor | E-commerce product page</title>
-     </Head>
-      <CartProvider>
-        <DataProvider>
-          <Component {...pageProps} />
-        </DataProvider>
-      </CartProvider>
-    </>
-  );
+
+   React.useEffect(() => {
+
+      let resizeTimer: any;
+      window.addEventListener("resize", () => {
+         document.body.classList.add("resize-animation-stopper");
+
+         clearTimeout(resizeTimer);
+
+         resizeTimer = setTimeout(() => {
+            document.body.classList.remove("resize-animation-stopper");
+         }, 400)
+      })
+
+   }, [])
+   return (
+      <>
+         <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Frontend Mentor | E-commerce product page</title>
+         </Head>
+         <CartProvider>
+            <DataProvider>
+               <Component {...pageProps} />
+            </DataProvider>
+         </CartProvider>
+      </>
+   );
 }
 export default MyApp;
