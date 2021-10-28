6bf4dafb22d4578f25a626b8cb601a171becd94d
avatarfreak <avatarfreak83@gmail.com>
2021-10-28 14:20:28 +0530

    stop animation on window resize

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Frontend Mentor | E-commerce product page</title>
-      </Head>
+     </Head>
       <CartProvider>
         <DataProvider>
           <Component {...pageProps} />
